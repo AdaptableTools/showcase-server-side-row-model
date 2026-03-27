cf4da38967aefa4cb5fa427c6ab68fe5b9744f82
radubrehar <radu@jslog.com>
2026-03-27 17:21:17 +0200

    remove old server

--- old-server/importData.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-import alasql from 'alasql';
-
-export default function importData() {
-  const sqlPath = path.resolve(__dirname, './data', 'olympic_winners.sql');
-  const dataSql = fs.readFileSync(sqlPath, 'utf-8');
-  alasql(dataSql);
-}
